42ea9f8384d375309dcd7f9469deab7c7ae315f1
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-05-14 23:07:35 +0200

    added pusher to the poll route

--- routes/poll.js
+++ routes/poll.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const Pusher = require('pusher')
+const pusher = new Pusher({
+    appId: "1203974",
+    key: "cee11e983a5b34f0291f",
+    secret: "7dad4bb232088ac176f9",
+    cluster: "eu",
+    useTLS: true
+  });
 
 router.get('/', (req, res)=>{
     res.send('poll')
 })
 
 router.post('/', (req,res)=>{
-    
+    pusher.trigger("os-poll", "os-vote", {
+        points: 1,
+        os: req.body.os
+      });
+      
+      return res.json({ success: true, message: 'Thank you for voting'})
 })
 
 module.exports = router 
