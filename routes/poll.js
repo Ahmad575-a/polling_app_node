ed0a7b3f8730bbef37f29df51d4165f6d09eb98b
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-05-15 19:59:46 +0200

    configure the db to save the votes after rendering

--- routes/poll.js
+++ routes/poll.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+
+const Vote = require('../models/Vote')
+
+
 const Pusher = require('pusher')
 const pusher = new Pusher({
     appId: "1203974",
@@ -10,16 +15,24 @@ const pusher = new Pusher({
   });
 
 router.get('/', (req, res)=>{
-    res.send('poll')
+    Vote.find().then(votes=> res.json({success: true, votes: votes}))
 })
 
 router.post('/', (req,res)=>{
-    pusher.trigger("os-poll", "os-vote", {
-        points: 1,
-        os: req.body.os
+    const newVote ={
+        os: req.body.os,
+        points: 1
+    }
+    new Vote(newVote).save().then(vote=>{
+        pusher.trigger("os-poll", "os-vote", {
+        points: parseInt(vote.points),
+        os: vote.os
       });
       
       return res.json({ success: true, message: 'Thank you for voting'})
+    })
+
+    
 })
 
 module.exports = router 
