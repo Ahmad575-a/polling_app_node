fa49300e285b26bf30db13f2cfbc0c0f06367271
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-07-13 21:43:55 +0200

    setup dotenv config

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
+require('dotenv').config()
 // DB config
 require('./config/db')
 
@@ -20,7 +20,7 @@ app.use(cors())
 
 app.use('/poll', poll)
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 // starting the server 
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`))
